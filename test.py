0d3d31fadfbfcbf7113816b89de5b570a72dd2df
k1nque <artkashaev@gmail.com>
2023-03-30 02:39:38 +0400

    test.py is deleted

--- test.py
+++
@@ -1,7 +0,0 @@
-from math import sin, cos, acos, pi
-from models.user import User
-
-
-
-print(User.getShortestDistance(40.70295166090133, -74.01600841261188, -37.82259314175813, 144.96886894504414))
-
