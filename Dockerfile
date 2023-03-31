b068553dddf85bae95c35cb16f6a3426164d1abc
k1nque <artkashaev@gmail.com>
2023-03-31 18:22:58 +0400

    fix

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM python:latest
 RUN pip3 install aiogram
 RUN pip3 install FlightRadarAPI
 
+CMD apt update && apt install nano iperf3
+
 ADD main.py /FlightRadarBot/
 ADD bot.py /FlightRadarBot/
 ADD config.py /FlightRadarBot/
@@ -11,8 +13,8 @@ ADD utility.py /FlightRadarBot/
 ADD models/user.py /FlightRadarBot/models/
 ADD SQL_scripts/database_init.sql /FlightRadarBot/SQL_scripts/
 
-ENV TOKEN=5996986939:AAFqhpZ03H7t1NJl-EYS7n7v72WK2mzI99A
+ENV TOKEN=token
 
 WORKDIR /FlightRadarBot/
 
-CMD python3 main.py -c && python3 main.py
+CMD python3 main.py
--- main.py
+++ main.py
@@ -6,21 +6,16 @@ import asyncio
 from flightDemon import startFlightDemon
 from utility import dp, dbPath
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--create-db', action='store_true')
-
 
     args = parser.parse_args()
 
-    if args.create_db:
-        conn = sqlite3.connect(dbPath)
-        cur = conn.cursor()
-        with open('SQL_scripts/database_init.sql', 'r') as file:
-            script = file.read()
-            cur.execute(script)
+    conn = sqlite3.connect(dbPath)
+    cur = conn.cursor()
+    with open('SQL_scripts/database_init.sql', 'r') as file:
+        script = file.read()
+        cur.execute(script)
 
-    else:
-        dp.loop.create_task(startFlightDemon())
-        startBot()
+    dp.loop.create_task(startFlightDemon())
+    startBot()
